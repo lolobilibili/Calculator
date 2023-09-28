18da1ec238b92bfdfe90a64f97fd9dec5a2da556
lolobilibili <victorcheng0831@gmail.com>
2023-09-28 15:18:49 +0800

    changed project name.

---
+++ build-calcuwork-Desktop_Qt_6_5_2_MinGW_64_bit-Debug/debug/moc_calculator.cpp
@@ -0,0 +1,282 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'calculator.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../Calcuwork/calculator.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'calculator.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSCalculatorENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSCalculatorENDCLASS = QtMocHelpers::stringData(
+    "Calculator",
+    "digitClicked",
+    "",
+    "unaryOperatorClicked",
+    "additiveOperatorClicked",
+    "multiplicativeOperatorClicked",
+    "equalClicked",
+    "pointClicked",
+    "changeSignClicked",
+    "backspaceClicked",
+    "copyClicked",
+    "pasteClicked",
+    "clear",
+    "clearAll",
+    "clearMemory",
+    "readMemory",
+    "setMemory",
+    "addToMemory"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSCalculatorENDCLASS_t {
+    uint offsetsAndSizes[36];
+    char stringdata0[11];
+    char stringdata1[13];
+    char stringdata2[1];
+    char stringdata3[21];
+    char stringdata4[24];
+    char stringdata5[30];
+    char stringdata6[13];
+    char stringdata7[13];
+    char stringdata8[18];
+    char stringdata9[17];
+    char stringdata10[12];
+    char stringdata11[13];
+    char stringdata12[6];
+    char stringdata13[9];
+    char stringdata14[12];
+    char stringdata15[11];
+    char stringdata16[10];
+    char stringdata17[12];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSCalculatorENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSCalculatorENDCLASS_t qt_meta_stringdata_CLASSCalculatorENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "Calculator"
+        QT_MOC_LITERAL(11, 12),  // "digitClicked"
+        QT_MOC_LITERAL(24, 0),  // ""
+        QT_MOC_LITERAL(25, 20),  // "unaryOperatorClicked"
+        QT_MOC_LITERAL(46, 23),  // "additiveOperatorClicked"
+        QT_MOC_LITERAL(70, 29),  // "multiplicativeOperatorClicked"
+        QT_MOC_LITERAL(100, 12),  // "equalClicked"
+        QT_MOC_LITERAL(113, 12),  // "pointClicked"
+        QT_MOC_LITERAL(126, 17),  // "changeSignClicked"
+        QT_MOC_LITERAL(144, 16),  // "backspaceClicked"
+        QT_MOC_LITERAL(161, 11),  // "copyClicked"
+        QT_MOC_LITERAL(173, 12),  // "pasteClicked"
+        QT_MOC_LITERAL(186, 5),  // "clear"
+        QT_MOC_LITERAL(192, 8),  // "clearAll"
+        QT_MOC_LITERAL(201, 11),  // "clearMemory"
+        QT_MOC_LITERAL(213, 10),  // "readMemory"
+        QT_MOC_LITERAL(224, 9),  // "setMemory"
+        QT_MOC_LITERAL(234, 11)   // "addToMemory"
+    },
+    "Calculator",
+    "digitClicked",
+    "",
+    "unaryOperatorClicked",
+    "additiveOperatorClicked",
+    "multiplicativeOperatorClicked",
+    "equalClicked",
+    "pointClicked",
+    "changeSignClicked",
+    "backspaceClicked",
+    "copyClicked",
+    "pasteClicked",
+    "clear",
+    "clearAll",
+    "clearMemory",
+    "readMemory",
+    "setMemory",
+    "addToMemory"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSCalculatorENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      16,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  110,    2, 0x08,    1 /* Private */,
+       3,    0,  111,    2, 0x08,    2 /* Private */,
+       4,    0,  112,    2, 0x08,    3 /* Private */,
+       5,    0,  113,    2, 0x08,    4 /* Private */,
+       6,    0,  114,    2, 0x08,    5 /* Private */,
+       7,    0,  115,    2, 0x08,    6 /* Private */,
+       8,    0,  116,    2, 0x08,    7 /* Private */,
+       9,    0,  117,    2, 0x08,    8 /* Private */,
+      10,    0,  118,    2, 0x08,    9 /* Private */,
+      11,    0,  119,    2, 0x08,   10 /* Private */,
+      12,    0,  120,    2, 0x08,   11 /* Private */,
+      13,    0,  121,    2, 0x08,   12 /* Private */,
+      14,    0,  122,    2, 0x08,   13 /* Private */,
+      15,    0,  123,    2, 0x08,   14 /* Private */,
+      16,    0,  124,    2, 0x08,   15 /* Private */,
+      17,    0,  125,    2, 0x08,   16 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject Calculator::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_CLASSCalculatorENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSCalculatorENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSCalculatorENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<Calculator, std::true_type>,
+        // method 'digitClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'unaryOperatorClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'additiveOperatorClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'multiplicativeOperatorClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'equalClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pointClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'changeSignClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'backspaceClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'copyClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pasteClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'clear'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'clearAll'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'clearMemory'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'readMemory'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'setMemory'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'addToMemory'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void Calculator::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Calculator *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->digitClicked(); break;
+        case 1: _t->unaryOperatorClicked(); break;
+        case 2: _t->additiveOperatorClicked(); break;
+        case 3: _t->multiplicativeOperatorClicked(); break;
+        case 4: _t->equalClicked(); break;
+        case 5: _t->pointClicked(); break;
+        case 6: _t->changeSignClicked(); break;
+        case 7: _t->backspaceClicked(); break;
+        case 8: _t->copyClicked(); break;
+        case 9: _t->pasteClicked(); break;
+        case 10: _t->clear(); break;
+        case 11: _t->clearAll(); break;
+        case 12: _t->clearMemory(); break;
+        case 13: _t->readMemory(); break;
+        case 14: _t->setMemory(); break;
+        case 15: _t->addToMemory(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *Calculator::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Calculator::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSCalculatorENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int Calculator::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 16)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 16;
+    }
+    return _id;
+}
+QT_WARNING_POP
